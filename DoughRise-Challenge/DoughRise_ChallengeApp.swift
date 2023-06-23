e5c053d71f1552ab64102b926f4a3653336034ce
bob-skywalker <q463250938@gmail.com>
2023-06-22 17:56:42 -0700

    Initial Commit

---
+++ DoughRise-Challenge/DoughRise_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoughRise_ChallengeApp.swift
+//  DoughRise-Challenge
+//
+//  Created by bo zhong on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DoughRise_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
